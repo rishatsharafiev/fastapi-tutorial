e2282880a89215162f9bdb66859d510fa7264059
Rishat Sharafiev <rishatsharafiev@ya.ru>
2020-10-12 09:13:13 +0300

    First steps 2

--- first_steps/main.py
+++ first_steps/main.py
@@ -1,14 +1,8 @@
-from typing import Mapping, Set
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-def f(my_mapping: Mapping[int, str]) -> Set[str]:
-    return set(my_mapping.values())
-
-
-a = f({3: "yes", 4: "no"})
-print(a)
-
-for i in range(5):  # this comment has too many spaces
-    print(i)
-    if True:
-        pass
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
